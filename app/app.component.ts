b1bddbd3841bd0975f6addebe6bbb7714af22f74
Toru Furukawa <torufurukawa@gmail.com>
2016-12-08 00:07:31 +0900

    Add data

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,18 @@
 import { Component } from '@angular/core';
 
+const HEROS: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'my-app',
   template: `
@@ -12,12 +25,10 @@ import { Component } from '@angular/core';
     </div>
   `,
 })
+
 export class AppComponent  {
   title = 'Tour of Heros';
-  hero = {
-    id: 1,
-    name: 'Wildstorm'
-  };
+  heros = HEROS;
 }
 
 export class Hero {
