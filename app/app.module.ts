be9b3b142e18186c6853b95ac1c5358f3e66f743
Toru Furukawa <torufurukawa@gmail.com>
2016-12-23 11:26:36 +0900

    Refactor routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,39 +1,21 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { RouterModule }  from '@angular/router';
 
 import { AppComponent }        from './app.component';
+import { DashboardComponent }  from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
-import { DashboardComponent }  from './dashboard.component';
 
+import { AppRoutingModule }    from './app-routing.module';
 
 
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      }
-    ])
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
@@ -41,10 +23,7 @@ import { DashboardComponent }  from './dashboard.component';
     HeroDetailComponent,
     HeroesComponent,
   ],
-  providers: [
-    HeroService
-  ],
-  bootstrap:    [ AppComponent ]
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule {
-}
+export class AppModule { }
